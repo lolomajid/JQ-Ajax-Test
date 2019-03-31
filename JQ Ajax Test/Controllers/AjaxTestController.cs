0ea7a3834de52a6c0edf5b088854e7454c541fcd
Ghajari.m <Ghajari.m@PC27>
2019-03-31 10:55:56 +0430

    Add project files.

---
+++ JQ Ajax Test/Controllers/AjaxTestController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JQ_Ajax_Test.Controllers
+{
+    public class AjaxTestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index38()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string Index38(string name,int id)
+        {
+            return name + " " + id.ToString();
+        }
+        [HttpGet]
+        public IActionResult Index39()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string Index39(string name, string family)
+        {
+            return name + " " + family;
+        }
+        [HttpGet]
+        public IActionResult Index40()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string Index40(string email)
+        {
+            if (email.ToUpper().Contains("@GMAIL.COM"))
+            {
+                return "Ok";
+            }
+            else
+            {
+                return "Not Ok";
+            }
+            
+        }
+        [HttpGet]
+        public IActionResult Index41()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string Index41(string email)
+        {
+            if (email.ToUpper().Contains("@GMAIL.COM"))
+            {
+                return "Ok";
+            }
+            else
+            {
+                return "Not Ok";
+            }
+
+        }
+    }
+}
---
+++ JQ Ajax Test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JQ_Ajax_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
